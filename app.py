abf377534c6accf60c6df576ccec938c73270299
Seyma SARIGİL <seymasargil@gmail.com>
2022-08-01 22:08:37 +0300

    data cleaning functions completed

--- app.py
+++ app.py
@@ -1,12 +1,47 @@
+import re
+import string
+
 import pandas as pd
-from flask import Flask, render_template, request, redirect, url_for, abort
+from flask import Flask, render_template, request, redirect, url_for, abort, send_file
 import os
 from werkzeug.utils import secure_filename
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk import word_tokenize
+import nltk
+
+nltk.download('stopwords')
+nltk.download('punkt')
+ps = PorterStemmer()
+stopwordSet = set(stopwords.words('turkish'))
 
 app = Flask(__name__)
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
 app.config['UPLOAD_EXTENSIONS'] = ['.xlsx']
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+app.config.update(
+    UPLOADED_PATH=os.path.join(dir_path, 'static/'),
+    DROPZONE_MAX_FILES=1
+)
+
+
+def stop_word(text):
+    text = word_tokenize(text, language='turkish')
+    text = [word for word in text if not word in stopwordSet]
+    text = " ".join(text)
+    return text
+
+
+def cleaning(text):
+    text = text.lower()
+    text = " ".join([word for word in text.split() if '#' not in word and '@' not in word])
+    text = re.sub(r"(\w+:\/\/\S+)|^rt|http.+?", " ", text)
+    translator = str.maketrans(' ', ' ', string.punctuation)
+    text = text.translate(translator)
+    text = re.sub("\d+", "", text)
+    return text
 
 
 @app.errorhandler(413)
@@ -24,7 +59,7 @@ def upload_file():
     my_files = request.files
     formData = request.form['myFormData']
 
-    with open("my_data.txt", "w+") as my_data:
+    with open("static/my_data.txt", "w+") as my_data:
         my_data.write(formData)
 
     for item in my_files:
@@ -36,7 +71,7 @@ def upload_file():
         if file_ext not in app.config['UPLOAD_EXTENSIONS']:
             abort(400)
 
-        uploaded_file.save(uploaded_file.filename)
+        uploaded_file.save(os.path.join(app.config['UPLOADED_PATH'] + 'data.xlsx'))
 
     return redirect(url_for('index'))
 
@@ -44,14 +79,16 @@ def upload_file():
 @app.route('/data_cleaning')
 def data_cleaning():
     try:
-        df = pd.read_excel('data.xlsx')
-        cleaning(df)
+        df = pd.read_excel('static/data2.xlsx')
+
+        textList = df.text.apply(cleaning)
+        textList = list(textList)
+
+        df['clean_data'] = textList
+        df.to_csv('static/clean_data.csv')
+
     except Exception as e:
         print('Alınan data temizlenirken bir hata oluştu', e)
     else:
-        return redirect(url_for('index'))
+        return send_file('static/clean_data.csv', as_attachment=True)
 
-
-def cleaning(df):
-    print('temizleme işleri')
-    print(df.head())
