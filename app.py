298ce0a78595d5d2fa528463d1046448da3745d8
Seyma SARIGİL <seymasargil@gmail.com>
2022-08-01 16:32:30 +0300

    data upload function completed.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
-
+import pandas as pd
+from flask import Flask, render_template, request, redirect, url_for, abort
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -15,3 +17,41 @@ def too_large(e):
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    my_files = request.files
+    formData = request.form['myFormData']
+
+    with open("my_data.txt", "w+") as my_data:
+        my_data.write(formData)
+
+    for item in my_files:
+        uploaded_file = my_files.get(item)
+        uploaded_file.filename = secure_filename(uploaded_file.filename)
+
+        if uploaded_file.filename != '':
+            file_ext = os.path.splitext(uploaded_file.filename)[1]
+        if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+            abort(400)
+
+        uploaded_file.save(uploaded_file.filename)
+
+    return redirect(url_for('index'))
+
+
+@app.route('/data_cleaning')
+def data_cleaning():
+    try:
+        df = pd.read_excel('data.xlsx')
+        cleaning(df)
+    except Exception as e:
+        print('Alınan data temizlenirken bir hata oluştu', e)
+    else:
+        return redirect(url_for('index'))
+
+
+def cleaning(df):
+    print('temizleme işleri')
+    print(df.head())
