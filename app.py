c3fb9fe0917e3fcab594f0967364d43252955cdd
Seyma SARIGİL <seymasargil@gmail.com>
2022-08-01 16:24:30 +0300

    the size of the uploaded file has been adjusted

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+app.config['UPLOAD_EXTENSIONS'] = ['.xlsx']
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+
+
+@app.errorhandler(413)
+def too_large(e):
+    return "Dosya boyutu 1GB tan fazla!", 413
+
 
 @app.route('/')
 def index():
